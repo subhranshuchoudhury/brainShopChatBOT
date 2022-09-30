8fea8b4f23d4b52cff94f23ea6c363e14082d1f6
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-09-30 11:44:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const { Protocol } = require('puppeteer');
+require('dotenv').config();
+const app = express();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => { res.header({ "Access-Control-Allow-Origin": "*" }); next(); });
+
+
+app.get("/", (req, res) => {
+    res.send({ status: 200, msg: "Working Fine!" });
+});
+
+
+// for whatsapp auto reply.
+
+app.post("/chatbot", async (req, res) => {
+
+    const message = req.body.message;
+    const app = req.body.app;
+    const sender = req.body.sender;
+    const group_name = req.body.group_name;
+    const phone = req.body.phone;
+
+    try {
+        const { data } = await axios.get(`http://api.brainshop.ai/get?bid=${process.env.bid}&key=${process.env.key}&uid=[uid]&msg=[${message}]`);
+        res.send({ reply: `@${phone} ${message}` });
+    } catch (error) {
+        res.send({ reply: "The AI BOT server is down! Kindly contact ADMIN @Butcher." });
+    }
+
+});
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Active on PORT 4000 ✅");
+});
