a261de7fdbde5ce65e3dfb9c7f636c1206fde9f4
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-09-30 12:07:19 +0530

    fix-2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.post("/chatbot", async (req, res) => {
 
-    const message = req.body.message;
+    const message = req.body.message.replace("cb", "");
     const app = req.body.app;
     const sender = req.body.sender;
     const group_name = req.body.group_name;
@@ -25,7 +25,7 @@ app.post("/chatbot", async (req, res) => {
 
     try {
         const { data } = await axios.get(`http://api.brainshop.ai/get?bid=${process.env.bid}&key=${process.env.key}&uid=[uid]&msg=[${message}]`);
-        res.send({ reply: `@${phone === undefined ? "BOT:" : phone} ${message}` });
+        res.send({ reply: `@${phone === undefined ? "BOT:" : phone} ${data.cnt}` });
     } catch (error) {
         res.send({ reply: "The AI BOT server is down! Kindly contact ADMIN @Butcher." });
     }
